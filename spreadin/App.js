d43d59ede056e8b36afbfe9c1d101d413c8e216f
hashan99 <hashankumarasinghe@gmail.com>
2021-08-30 22:12:58 +0530

    app.js edited

--- spreadin/App.js
+++ spreadin/App.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, } from 'react-native';
 import logo from './assets/logo.png';
 
 export default class App extends React.Component {
+
+  state = {
+    value : 0
+  }
+
+  addValue = () => {
+    this.setState({value : this.state.value +10})
+  }
+
+  subValue = () => {
+    this.setState({value : this.state.value -10})
+  }
+
+  resetValue = () => {
+    this.setState({value : 0})
+  }
+
   render() {
     return (
       <View style={styles.container}>
+
+        <Text style={{color: 'white', fontSize: 24, fontWeight: 'bold'}}>Spreadin</Text>
+
         <Image source={logo} style={styles.logo} />
-        <Text style={{color : 'white', fontSize : 24}}>Spreadin</Text>
-        {/* <Text style={{color : 'white'}}>Aditional text here</Text> */}
+
+        <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}> 
+          
+          <TouchableOpacity style={styles.operator} onPress={() => this.subValue()}>
+            <Text style={{color: 'black', fontSize: 16, fontWeight: 'bold'}}> - </Text>
+          </TouchableOpacity>
+
+          <Text style={(this.state.value == 0) ? {color: 'white', fontSize: 16, fontWeight: 'bold'} : {color: '#008b8b', fontSize: 16, fontWeight: 'bold'}}> {this.state.value} </Text>
+
+          <TouchableOpacity style={styles.operator} onPress={() => this.addValue()}>
+            <Text style={{color: 'black', fontSize: 16, fontWeight: 'bold'}}> + </Text>
+          </TouchableOpacity>
+
+        </View>
+
+        {
+          (this.state.value == 0)
+          ?
+          <Text style={{color: 'white', fontSize: 16}}>You can increment or decrement the value!</Text>
+          :
+          <TouchableOpacity style={styles.button} onPress={() => this.resetValue()}>
+          <Text style={{color: 'white', fontSize: 16}}>Reset to Zero</Text>
+        </TouchableOpacity>
+        }
+
         <StatusBar style="auto" />
+
       </View>
     );
   }
@@ -27,6 +71,22 @@ const styles = StyleSheet.create({
   logo: {
     width: 100,
     height: 100,
-    marginBottom: 12,
+    margin: 12,
   },
+  button: {
+    backgroundColor: '#008b8b', 
+    paddingHorizontal: 20, 
+    paddingVertical: 10, 
+    borderRadius: 10, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    margin: 12,
+  },
+  operator: {
+    backgroundColor: 'white', 
+    paddingVertical: 8, 
+    paddingHorizontal: 10,
+    margin: 16, 
+    borderRadius: 10
+  }
 });
