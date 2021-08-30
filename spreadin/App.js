62bf60c44a39f00c968b53766702f6dd926c5867
hashan99 <hashankumarasinghe@gmail.com>
2021-08-30 14:19:08 +0530

    app re arranged

--- spreadin/App.js
+++ spreadin/App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import logo from './assets/logo.png';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={logo} style={styles.logo} />
+        <Text style={{color : 'white', fontSize : 24}}>Spreadin</Text>
+        {/* <Text style={{color : 'white'}}>Aditional text here</Text> */}
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 12,
   },
 });
